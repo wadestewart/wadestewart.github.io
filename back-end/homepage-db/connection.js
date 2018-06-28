a2a5c2fd69bbd760f2eadf7e6d78e2f047c3154d
Wade Stewart <stew.wade@gmail.com>
2018-06-28 10:10:26 -0400

    fixed mongoose connection error

--- back-end/homepage-db/connection.js
+++ back-end/homepage-db/connection.js
@@ -2,10 +2,10 @@ const mongoose = require('./models/person')
 
 mongoose.Promise = Promise
 
-const mongoUri = 'mongodb://localhost/person'
+const mongoUri = 'mongodb://localhost:27017/person'
 
 mongoose
-    .connect(mongoUri)
+    .createConnection(mongoUri)
     .then(connection => console.log(`Connection established to db '${connection.db.databaseName}'`))
     .catch(connectionError => console.log('Connection failed!', connectionError))
 
