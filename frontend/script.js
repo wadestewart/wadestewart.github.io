933483bb0846eae399046882f5bc3ac70dd0fb1b
Wade Stewart <stew.wade@gmail.com>
2018-07-01 18:37:54 -0400

    button flickering animiation added

--- frontend/script.js
+++ frontend/script.js
@@ -1,42 +1,3 @@
-// let pin = document.getElementsByClassName('pin')
-
-// container.addEventListener('click', function(){
-//     console.log('clicked')
-// })
-
-// function updatePinPosition(){
-    
-//     if (window.innerWidth/window.innerHeight >= 2600/1450) {
-//         setStyle('container', {
-//             'height': '100%',
-//             'width': container.innerHeight*(2600/1450)+'px',
-//             'margin-left': -container.innerWidth/2+'px',
-//             'margin-top': -container.innerHeight/2+'px'
-//         })
-//     } else {
-//         setStyle('container', {
-//             'height': container.innerWidth*(1450/2600)+'px',
-//             'width': '100%',
-//             'margin-left': 0,
-//             'left': 0,
-//             'top': '50%',
-//             'margin-top': -container.innerHeight/2+'px'
-//         })
-//     }
-    
-//     function setStyle(objId, propertyObject){
-//         let elem = document.getElementById(objId)
-//         for(let property in propertyObject)
-//         elem.style[property] = propertyObject[property]
-//     }
-
-//     // console.log(container.style)
-// }
-
-// document.addEventListener('load', updatePinPosition())
-
-// window.addEventListener('load', updatePinPosition())
-
 let container = $('#container')
 let button = $('#button')
 
@@ -48,6 +9,8 @@ $(window).resize(function(){
     updatePinPosition()
 })
 
+// This is keep the button pinned to the surge protector
+// https://stackoverflow.com/questions/30996097/divs-placed-relative-to-image-on-window-resize-almost-works
 function updatePinPosition() {
     if ($(window).width()/$(window).height() >= (2600/1450)) {
         container.css({
