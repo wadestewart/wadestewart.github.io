ba3bd5ade0dacbaf2a9195098cd9d1ced9fc91f9
Wade Stewart <stew.wade@gmail.com>
2018-08-03 16:39:38 -0400

    working on scroll effect and changinf navbar

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let hamburger = $('.hamburger')
-let sidenav = $('#mySidenav')
-let close = $('.closebtn')
-
-
-hamburger.on('click', function() {
-    sidenav.css({ 'width': '100%' })
-})
-
-sidenav.on('click', function () {
-    sidenav.css({ 'width': '0'})
-})
 
