84518320fd27075cdcd704426ed1682359bbcc84
Wade Stewart <stew.wade@gmail.com>
2018-08-03 20:59:33 -0400

    buggy scroll event

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+// This scroll code (and amazing explanation) is from https://stackoverflow.com/questions/21450095/how-to-make-mouse-wheel-scroll-to-section-like-in-mediafire-com
+
+$('section').height($(window).height)
+$('section').first().addClass('active')
+
+$(document).on('mousewheel DOMMouseScroll', function(e) {
+    e.preventDefault()
+    let active = $('section.active')
+    let delta = e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0 ? 1 : -1
+
+    if (delta < 0) {
+        next = active.next()
+
+        if (next.length) {
+            let timer = setTimeout(function() {
+                $('html, body').animate({
+                    scrollTop: next.offset().top
+                }, 'slow')
+
+            next.addClass('active')
+                .siblings().removeClass('active')
+
+            clearTimeout(timer)
+            }, 800)
+        }
+    } 
+    // else {
+    //     prev = active.prev()
+
+    //     if (prev.length) {
+
+    //     }
+    // }
+})
 
