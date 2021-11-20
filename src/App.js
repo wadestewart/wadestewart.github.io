08c20c726b31d845255409a8f93d081f3a6d648a
Wade Stewart <stew.wade@gmail.com>
2021-11-20 12:31:23 -0500

    CRA, rendering img

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import wade from './img/transparent-lander.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={wade} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
